3507b45ee69b50b571259a97b17bc16bdc5a554a
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-19 14:25:48 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//First we need three variables to hold the location of the three sections of the ship.
+// Then declare a variable to hold the user's current guess.
+//Declare a variable to hold the number of hits.
+//Declare a variable to hold the number of guesses.
+//Declare a variable to keep rack of whether the ship is sunk or not.
+
+const loc1 = 3;
+const loc2 = 4;
+const loc3 = 5;
+
+const guess;
+const hits = 0;
+const numberOfGuesses = 0;
+
+const isSunk = false;
+
+while (isSunk === false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+}
